786d4d4e97cf3f632cce9c8fb94018ba79a6099c
Kade Griffith <kadeg95@gmail.com>
2021-08-18 11:03:27 -0600

    form validation workds with yup, post request is successful

--- advancedform/src/Form.js
+++ advancedform/src/Form.js
@@ -1,35 +1,112 @@
 import axios from 'axios';
-import * as yup from 'yup';
+import * as Yup from 'yup';
 import {useState, useEffect} from 'react';
 
 const Form = (props) => {
-const [formData, setFormData] = useState({
-    username: '',
-    email:'',
-    password:'',
-    agreedTOS: false,
-    buttonDisabled: true,
-});
+    const [formData, setFormData] = useState({
+        username: '',
+        email:'',
+        password:'',
+        agreedTOS: false,
+    });
+    const [buttonDisabled, setButtonDisabled] = useState(true);
+    const [errors, setErrors] = useState({
+        username: '',
+        email: '',
+        password: '',
+        agreedTOS: '',
+    });
+    const [users, setUsers] = useState([]);
+
+    const formSchema = Yup.object().shape({
+        username: Yup
+            .string()
+            .trim()
+            .required("Must include a username.")
+            .min(6, "Username must be at least 6 characters long."),
+        email: Yup
+          .string()
+          .trim()
+          .email("Must be a valid email address.")
+          .required("Must include email address."),
+        password: Yup
+          .string()
+          .trim()
+          .required("Password is Required")
+          .min(8, "Passwords must be at least 8 characters long."),
+        agreedTOS: Yup
+          .boolean()
+          .oneOf([true], "You must accept Terms and Conditions")
+      });
+
+      useEffect( () => {
+        formSchema.isValid(formData).then((valid) => {
+            setButtonDisabled(!valid);
+        })
+      }, [formData])
+
+      const inputChange = e => {
+          const {name, value, checked, type} = e.target
+          const valueToUse = type === 'checkbox' ? checked : value;
+
+          Yup
+          .reach(formSchema, name)
+          .validate(value)
+          .then(valid => {
+              setErrors({
+                  ...errors, [name]: ''
+              })
+          })
+          .catch(err => {
+              setErrors({
+                  ...errors, [name]: err.errors[0]
+              })
+          })
+          setFormData({
+              ...formData, [name]: valueToUse
+          })
+        }
+
+     const submit = (e) => {
+         e.preventDefault();
+         const newUser = {username: formData.username, email: formData.email, password: formData.password, agreedTOS: formData.agreedTOS}
+         axios.post('https://reqres.in/api/users', newUser)
+         .then(res => {
+             console.log(res);
+             setFormData(...{
+                username: '',
+                email:'',
+                password:'',
+                agreedTOS: false,
+            });
+         })
+         .catch(err => {
+             debugger
+         })
+     }
 
     return (
-        <form>
+        <form onSubmit={submit}>
             <label>
                 Username
-                <input id='username' type='text' name='username' placeholder='Enter a Username'></input>
+                <input onChange={inputChange} id='username' type='text' name='username' value={formData.username} placeholder='Enter a Username'></input>
             </label>
+            {errors.username.length > 0 && <p>{errors.username}</p>}
             <label>
                 Email
-                <input id='email' type='email' name='email' placeholder='Enter your Email'></input>
+                <input onChange={inputChange} id='email' type='email' name='email' value={formData.email} placeholder='Enter your Email'></input>
             </label>
+            {errors.email.length > 0 && <p>{errors.email}</p>}
             <label>
                 Password
-                <input id='password' type='password' name='password' placeholder='Enter a Secure Password'></input>
+                <input onChange={inputChange} id='password' type='password' name='password' value={formData.password} placeholder='Enter a Secure Password'></input>
             </label>
+            {errors.password.length > 0 && <p>{errors.password}</p>}
             <label>
                 Terms and Conditions
-                <input id='tos' type='checkbox' name='agreedTOS'></input>
+                <input onChange={inputChange} id='tos' type='checkbox' name='agreedTOS' checked={formData.agreedTOS} ></input>
             </label>
-            <button disabled={formData.buttonDisabled} >Submit</button>
+            <button disabled={buttonDisabled} >Submit!</button>
         </form>
     )
 }
